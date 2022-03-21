1cd3092a7bd36c702c7bf3ab35fcba83683db0b4
Itallo Pacheco <itallo.santos@academico.ifs.edu.br>
2022-03-20 22:06:11 -0300

    [📜] listando agendamentos (#H05)

--- agendamento/admin.py
+++ agendamento/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from agendamento.models import Estabelecimento
+from agendamento.models import Estabelecimento, Agendamento, Agendamento_Account
 # Register your models here.
 
 @admin.register(Estabelecimento)
@@ -8,5 +8,23 @@ class EstabelecimentoAdmin(admin.ModelAdmin):
     list_display = ('id', 'cnes', 'nome',)
     search_fields = ('cnes', 'nome',)
     ordering = ('nome',)
+    filter_horizontal = ()
+    fieldsets = ()
+
+@admin.register(Agendamento)
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_filter = ('estabelecimento', 'data_agendamento', 'account')
+    list_display = ('id', 'estabelecimento', 'data_agendamento', 'dia_extenso',)
+    search_fields = ('estabelecimento', 'data_agendamento',)
+    ordering = ('estabelecimento',)
+    filter_horizontal = ()
+    fieldsets = ()
+
+@admin.register(Agendamento_Account)
+class Agendamento_Account(admin.ModelAdmin):
+    list_filter = ('account', 'agendamento', 'hora', 'is_active')
+    list_display = ('account', 'agendamento', 'hora', 'is_active', 'status')
+    search_fields = ('account',)
+    ordering = ('account',)
     filter_horizontal = ()
     fieldsets = ()
--- agendamento/models.py
+++ agendamento/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from account.models import Account
+from datetime import date
+from django.utils.timezone import localtime
 # Create your models here.
 
 class Estabelecimento(models.Model):
@@ -10,3 +12,54 @@ class Estabelecimento(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Agendamento(models.Model):
+    estabelecimento = models.ForeignKey(Estabelecimento, on_delete=models.CASCADE, verbose_name='Estabelecimento')
+    data_agendamento = models.DateField(verbose_name='Data Agendamento')
+    account = models.ManyToManyField(Account, through='Agendamento_Account')
+
+    def __str__(self):
+        return f"{self.estabelecimento.nome}, {self.data_agendamento}, {self.dia_extenso}"
+
+    @property
+    def dia_extenso(self):
+        dias_semana = {0: 'Segunda-Feira',
+                       1: 'Terça-Feira',
+                       2: 'Quarta-Feira',
+                       3: 'Quinta-Feira',
+                       4: 'Sexta-Feira',
+                       5: 'Sábado',
+                       6: 'Domingo'}
+
+        agendamento = dias_semana[self.data_agendamento.weekday()]
+        return agendamento
+
+
+class Agendamento_Account(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    agendamento = models.ForeignKey(Agendamento, on_delete=models.CASCADE)
+    hora = models.TimeField(default='00:00', verbose_name='Hora do agendamento')
+    is_active = models.BooleanField(verbose_name='esta ativo', default=True, null=False)
+
+    @property
+    def status(self):
+        if date.today() == self.agendamento.data_agendamento and localtime().time() > self.hora:
+            return 'Encerrado'
+        else:
+            if date.today() > self.agendamento.data_agendamento:
+                return 'Encerrado'
+        return 'Ativo'
+
+    @property
+    def dia_extenso(self):
+
+        dias_semana = {0: 'Segunda-Feira',
+                       1: 'Terça-Feira',
+                       2: 'Quarta-Feira',
+                       3: 'Quinta-Feira',
+                       4: 'Sexta-Feira',
+                       5: 'Sábado',
+                       6: 'Domingo'}
+
+        agendamento = dias_semana[self.agendamento.data_agendamento.weekday()]
+        return agendamento
